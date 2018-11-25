93ef2f3cacd7e7b42f3ad706a9d4b877b1a4ff35
ftvalentini <ft.valentini@gmail.com>
2018-11-25 13:16:39 -0300

    saldos [no recursion]

--- 06-update_saldos.py
+++ 06-update_saldos.py
@@ -18,12 +18,20 @@ for n in nombres:
     saldos[n] = pf.saldo_history_user(txs_df=txs, user_name=n, frec='d')
 
 #%% check: sum of saldos equals zero each period
-def add_recursive(df_list):
-    if len(df_list)==1:
-        return df_list[0]
-    else:
-        return df_list[0] + add_recursive(df_list[1:])
-balance_global = add_recursive(df_list=saldos.tolist()).balance_cum.astype(int)
+# recursion no funciona porque "In Python, recursion is limited to 999 calls"
+# def add_recursive(df_list):
+#     if len(df_list)==1:
+#         return df_list[0]
+#     else:
+#         return df_list[0] + add_recursive(df_list[1:])
+def add_dfs(df_list):
+    df = df_list[0]
+    i = 1
+    while i<len(df_list):
+        df = df + df_list[i]
+        i = i+1
+    return df
+balance_global = add_dfs(df_list=saldos.tolist()).balance_cum.astype(int)
 if (balance_global!=0).any():
     sys.exit("Sum of balances not equal to 0 in " + str(balance_global.index[balance_global!=0][0]))
 
