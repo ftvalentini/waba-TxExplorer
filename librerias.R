1aaff59b430a42722d402d89d8ca73aab42ea476
ftvalentini <ft.valentini@gmail.com>
2019-04-24 22:35:09 -0300

    set repository in install packages

--- librerias.R
+++ librerias.R
@@ -1,24 +1,29 @@
 
 # escribir paquetes "desiempre" y "proj" ---------------------------------------
-  # las desiempre no se llaman explicitamente en los Scripts, las proj sí
-paq_desiempre <- c("magrittr",
-                   "purrr",
-                   "conflicted",
-                   "ggplot2",
-                   "dplyr",
-                   "stringr",
-                   "broom",
-                   "knitr",
-                   "readr",
-                   "kableExtra",
-                   "DT")
-paq_proj <- c("bsselectR")
+# las desiempre no se llaman explicitamente en los Scripts, las proj sí
+  paq_desiempre <- c(
+    "magrittr",
+    "purrr",
+    "conflicted",
+    "ggplot2",
+    "dplyr",
+    "stringr",
+    "broom",
+    "knitr",
+    "readr",
+    "kableExtra",
+    "rmarkdown",
+    "DT"
+  )
+paq_proj <- c(
+  )
 
 # no tocar ----------------------------------------------------------------
 paq <- c(paq_desiempre,paq_proj)
 for (i in seq_along(paq)) {
   if (!(paq[i] %in% installed.packages()[,1])) {
-    install.packages(paq[i], quiet=T, verbose=F)
+    install.packages(paq[i], quiet=T, verbose=F,
+      repos="http://cran.us.r-project.org")
   }
   if (paq[i] %in% paq_desiempre) library(paq[i],character.only=T,
                                          quietly=T,warn.conflicts=F,verbose=F)
